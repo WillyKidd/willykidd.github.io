36b022799edbbc934fcb0a48c9f51a68c12f347e
weili <a320_271N@outlook.com>
2023-03-19 17:03:01 +0800

    fix: gh deployment configs

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/" : "/"
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   publicPath: process.env.NODE_ENV === "production" ? "/" : "/"
+// })
+
+module.exports = {
+  publicPath: '/'
+}
